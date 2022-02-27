df059cc1a16660c68b534042721ac7dba437b933
Alexey Golubev <dr.freecx@gmail.com>
2022-02-27 21:36:31 +0300

    feat: add buffer reader

---
+++ src/reader.rs
@@ -0,0 +1,52 @@
+#![allow(unused_variables, dead_code)]
+use std::iter::Peekable;
+use std::str::Chars;
+
+pub struct StringReader<'a> {
+    src: Peekable<Chars<'a>>,
+}
+
+impl<'a> StringReader<'a> {
+    pub fn new(src: &'a str) -> StringReader {
+        StringReader { src: src.chars().peekable() }
+    }
+
+    pub fn peek(&mut self) -> Option<&char> {
+        self.src.peek()
+    }
+
+    pub fn take(&mut self) -> Option<char> {
+        self.src.next()
+    }
+
+    pub fn drop(&mut self) {
+        let _ = self.take();
+    }
+
+    pub fn take_while<F>(&mut self, f: F) -> Option<String>
+    where
+        F: Fn(&char) -> bool,
+    {
+        let mut buffer = Vec::new();
+        while let Some(value) = self.src.peek() {
+            if !f(value) {
+                break;
+            }
+            let item = self.src.next().unwrap();
+            buffer.push(item);
+        }
+        Some(buffer.iter().collect::<String>())
+    }
+
+    pub fn drop_while<F>(&mut self, f: F)
+    where
+        F: Fn(&char) -> bool,
+    {
+        while let Some(value) = self.src.peek() {
+            if !f(value) {
+                break;
+            }
+            let _ = self.src.next();
+        }
+    }
+}
